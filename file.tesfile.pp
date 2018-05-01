b5925fbc786b867cfe6061b319c2893ba7f71c95
Jusong Chen <jusong.chen@gmail.com>
2018-05-01 11:58:04 -0700

    change mode setting on file

--- file.tesfile.pp
+++ file.tesfile.pp
@@ -1,6 +1,7 @@
 file { '/tmp/testfile.txt':
  ensure => present,
- mode => '0644',
- replace => true,
+ #mode => '0644', #
+ mode => 'u=rw,g=r,o=r',
+ replace => true,  #when set to false, the content won't be changed and no error message wil be reported!
  content => "wow cow!\n", #use double quote so that \n is interpreted
 }
