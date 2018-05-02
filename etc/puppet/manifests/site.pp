3b07bc6dfd7564b2ee408669361b24feea4f4d23
Jusong Chen <jusong.chen@gmail.com>
2018-05-02 14:00:48 -0700

    add node classification to site.pp

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -1,4 +1,17 @@
+
+File {
+ mode => '0600',
+}
+
 node 'ip-10-0-0-225.us-west-1.compute.internal' {
 	include users
+  #include users - deploy modules users to agent specified by the node
 }
-#include users - deploy modules users to agent specified by the node
+
+node /.*\.us-west.*\.internal$/ {
+  include ntp
+}
+
+node default {
+  notify { "${clientcert} is not classified": }
+}
