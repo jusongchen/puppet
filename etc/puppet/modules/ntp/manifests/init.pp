2f2c1e404b8ea0729a1fabfc2b24495c73f50993
Jusong Chen <jusong.chen@gmail.com>
2018-05-02 15:23:42 -0700

    modify dependency

--- etc/puppet/modules/ntp/manifests/init.pp
+++ etc/puppet/modules/ntp/manifests/init.pp
@@ -7,12 +7,13 @@ class ntp {
     default => 'ntpd',
   }
 
-  Package ['ntp']-> File ['/etc/ntp.conf']~>Service['ntpd']
-# ~> notify/subscribe relationship
-# -> before/require relationshipt
+  #this is not flexible as ntpd will be ntp on Ubuntu osfamily
+  # Package ['ntp']-> File ['/etc/ntp.conf']~>Service['ntpd']
+
+
   package { 'ntp':
     ensure => present,
-  }
+  }-> # -> before/require relationshipt
   file {'/etc/ntp.conf':
     ensure => file,
     owner => 'root',
@@ -20,7 +21,7 @@ class ntp {
     mode => '0644',
    # source => 'puppet:///modules/ntp/ntp.conf',
     content => template('ntp/ntp.conf.erb'),
-  }
+  }~>  # ~> notify/subscribe relationship
   service {$ntp_srv_name:
     ensure => running,
     enable => true, #this will tell system to start this service when reboot
