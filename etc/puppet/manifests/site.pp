251c8038902ffc129a3db217bccaa6f637879a27
Jusong Chen <jusong.chen@gmail.com>
2018-05-02 14:06:42 -0700

    add notifiatoin messages

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -4,12 +4,13 @@ File {
 }
 
 node 'ip-10-0-0-225.us-west-1.compute.internal' {
-	include users
-  #include users - deploy modules users to agent specified by the node
+	include users # deploy modules users to agent specified by the node
+  notify { "${clientcert} is matched to deploy module users": }
 }
 
 node /.*\.us-west.*\.internal$/ {
   include ntp
+  notify { "${clientcert} is matched to deploy ntp": }
 }
 
 node default {
