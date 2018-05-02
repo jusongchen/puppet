ec0a16352b840030aaef7813ce66df389014d6c5
Jusong Chen <jusong.chen@gmail.com>
2018-05-02 15:18:49 -0700

    use function to deploy ntp service on Ubuntu and CentOS

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -5,14 +5,15 @@ File {
 
 # evaluated as case statement - only the first matched one get executed!
 
-node /.*\.us-west.*\.internal$/ {
-  include ntp
-  notify { "${clientcert} is matched to deploy ntp": }
-}
+# node /.*\.us-west.*\.internal$/ {
+#   include ntp
+#   notify { "${clientcert} is matched to deploy ntp": }
+# }
 
-node 'ip-10-0-0-225.us-west-1.compute.internal' {
+node /ip-10-0-0-.*us-west-1.compute.internal/ {
 	include users # deploy modules users to agent specified by the node
-  notify { "${clientcert} is matched to deploy module users": }
+  include ntp
+  notify { "${clientcert} is matched to regexp /ip-10-0-0-.*us-west-1.compute.internal": }
 }
 
 node default {
--- etc/puppet/modules/ntp/manifests/init.pp
+++ etc/puppet/modules/ntp/manifests/init.pp
@@ -1,6 +1,12 @@
 class ntp {
   $ntp_servers=['time.apple.com', 'us.pool.ntp.org', 'time.nist.gov']
 
+  $ntp_srv_name = $::osfamily ? {
+    'RedHat' => 'ntpd',
+    'Debian' => 'ntp',
+    default => 'ntpd',
+  }
+
   Package ['ntp']-> File ['/etc/ntp.conf']~>Service['ntpd']
 # ~> notify/subscribe relationship
 # -> before/require relationshipt
@@ -15,7 +21,7 @@ class ntp {
    # source => 'puppet:///modules/ntp/ntp.conf',
     content => template('ntp/ntp.conf.erb'),
   }
-  service {'ntpd':
+  service {$ntp_srv_name:
     ensure => running,
     enable => true, #this will tell system to start this service when reboot
   }
