0dbbf8ef36db26c0421e4a2d525b2050470df3c6
Jusong Chen <jusong.chen@gmail.com>
2018-05-02 10:10:49 -0700

    variable in single quote sting will not be interpreted

--- basic_manifests/notify.greeting.pp
+++ basic_manifests/notify.greeting.pp
@@ -1,4 +1,4 @@
 
 notify { 'greeting':
-   message => 'Hello, world! your OS family is $::osfamily',
+   message => "Hello, world! your OS family is $::osfamily", #variable in single quote string will be not evaluated!
 }
